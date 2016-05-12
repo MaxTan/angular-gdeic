64dbc7ed089f079d7eb24777bb8b70721959bd55
xie.ys <alchemize47ys@hotmail.com>
2016-05-12 15:41:14 +0800

    add prototype extentions

---
+++ dist/angular-gdeic.js
@@ -0,0 +1,332 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	(function (angular) {
+	    var ngApp = angular.module('ngGdeic', ['ngAnimate', 'ui.bootstrap', 'angular-linq']);
+
+	    __webpack_require__(1)(angular);
+
+	} (angular));
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = function (angular) {
+
+	    __webpack_require__(2)(angular);
+	    __webpack_require__(3)(angular);
+	    __webpack_require__(4)(angular);
+	    __webpack_require__(5)(angular);
+
+	}
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	module.exports = function (angular) {
+	    'use strict';
+
+	    (function (_) {
+	        _.defineProperties(_.prototype, {
+	            formatDate: {
+	                value: function formatDate() {
+	                    var i, max, p, value;
+	                    if (angular.isArray(this)) {
+	                        for (i = 0, max = this.length; i < max; i++) {
+	                            value = this[i];
+	                            if (angular.isObject(value)) {
+	                                value.formatDate();
+	                            }
+	                        }
+	                    } else {
+	                        for (p in this) {
+	                            if (this.hasOwnProperty(p)) {
+	                                value = this[p];
+	                                if (angular.isString(value) && /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/.test(value)) {
+	                                    if (value === '1900-01-01T00:00:00' || value === '0001-01-01T00:00:00') {
+	                                        delete this[p];
+	                                    } else {
+	                                        this[p] = new Date(this[p]);
+	                                    }
+	                                } else if (angular.isObject(value)) {
+	                                    value.formatDate();
+	                                }
+	                            }
+	                        }
+	                    }
+	                },
+	                writable: false,
+	                enumerable: false
+	            },
+	            clearProperties: {
+	                value: function clear() {
+	                    var p, value;
+	                    if (angular.isArray(this)) {
+	                        this.splice(0, this.length);
+	                    } else {
+	                        for (p in this) {
+	                            if (this.hasOwnProperty(p)) {
+	                                value = this[p];
+	                                if (angular.isObject(value)) {
+	                                    this[p].clearProperties();
+	                                } else {
+	                                    this[p] = null;
+	                                }
+	                            }
+	                        }
+	                    }
+	                },
+	                writable: false,
+	                enumerable: false
+	            },
+	            isClear: {
+	                value: function () {
+	                    var p, value;
+	                    if (angular.isArray(this)) {
+	                        if (this.length > 0) {
+	                            return false;
+	                        }
+	                    } else {
+	                        for (p in this) {
+	                            if (this.hasOwnProperty(p)) {
+	                                value = this[p];
+	                                if (angular.isObject(value)) {
+	                                    if (!value.isClear()) {
+	                                        return false;
+	                                    }
+	                                } else if (!angular.isDate(value)) {
+	                                    if (angular.isDefined(value) && value !== null) {
+	                                        return false;
+	                                    }
+	                                }
+	                            }
+	                        }
+	                    }
+	                    return true;
+	                },
+	                writable: false,
+	                enumerable: false
+	            }
+	        });
+	    } (Object));
+	};
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	module.exports = function (angular) {
+	    'use strict';
+
+	    (function (_) {
+	        Object.defineProperties(_.prototype, {
+	            intersect: {
+	                value: function intersect(arr) {
+	                    var array = [], i = 0, max = this.length, currItem;
+	                    arr = arr.map(function (item) {
+	                        return angular.toJson(item);
+	                    });
+	                    for (; i < max; i++) {
+	                        currItem = angular.toJson(this[i]);
+	                        if (arr.indexOf(currItem) > -1) {
+	                            array.push(angular.copy(currItem));
+	                        }
+	                    }
+	                    return array.map(function (item) {
+	                        return angular.fromJson(item);
+	                    });
+	                },
+	                writable: false,
+	                enumerable: false
+	            },
+	            differentiate: {
+	                value: function difference(arr) {
+	                    var array = [], i = 0, max = this.length, currItem;
+	                    arr = arr.map(function (item) {
+	                        return angular.toJson(item);
+	                    });
+	                    for (; i < max; i++) {
+	                        currItem = angular.toJson(this[i]);
+	                        if (arr.indexOf(currItem) < 0) {
+	                            array.push(angular.copy(currItem));
+	                        }
+	                    }
+	                    return array.map(function (item) {
+	                        return angular.fromJson(item);
+	                    });
+	                },
+	                writable: false,
+	                enumerable: false
+	            }
+	        });
+	    } (Array));
+	};
+
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
+
+	module.exports = function (angular) {
+	    'use strict';
+
+	    (function (_) {
+	        Object.defineProperties(_.prototype, {
+	            trimAll: {
+	                value: function trimAll() {
+	                    return this.replace(/\s/g, '');
+	                },
+	                writable: false,
+	                enumerable: false
+	            },
+	            padStart: {
+	                value: function padStart(length, string) {
+	                    string = string || ' ';
+	                    if (length <= this.length) {
+	                        return this;
+	                    }
+
+	                    var padLength = length - this.length;
+	                    if (padLength <= string.length) {
+	                        return string.substring(0, padLength) + this;
+	                    } else {
+	                        var len = Math.floor(padLength / string.length), i = 0, str = '';
+	                        while (i < len) {
+	                            str += string;
+	                            i++;
+	                        }
+	                        return str + string.substring(0, padLength - len * string.length) + this;
+	                    }
+	                },
+	                writable: false,
+	                enumerable: false
+	            },
+	            padEnd: {
+	                value: function padEnd(string) {
+	                    string = string || ' ';
+	                    if (length <= this.length) {
+	                        return this;
+	                    }
+
+	                    var padLength = length - this.length;
+	                    if (padLength <= string.length) {
+	                        return this + string.substring(0, padLength);
+	                    } else {
+	                        var len = Math.floor(padLength / string.length), i = 0, str = '';
+	                        while (i < len) {
+	                            str += string;
+	                            i++;
+	                        }
+	                        return this + str + string.substring(0, padLength - len * string.length);
+	                    }
+	                },
+	                writable: false,
+	                enumerable: false
+	            }
+	        });
+	    } (String));
+	};
+
+/***/ },
+/* 5 */
+/***/ function(module, exports) {
+
+	module.exports = function (angular) {
+	    'use strict';
+
+	    (function (_) {
+	        Object.defineProperties(_.prototype, {
+	            getQuarter: {
+	                value: function getQuarter() {
+	                    return Math.ceil((this.getMonth() + 1) / 3);
+	                },
+	                writable: false,
+	                enumerable: false
+	            },
+	            toDateString: {
+	                value: function toDateString(format) {
+	                    if (angular.isUndefined(format)) {
+	                        return this.toString();
+	                    }
+
+	                    if (/y{4}/.test(format)) {
+	                        format = format.replace(/y{4}/g, this.getFullYear());
+	                    }
+	                    if (/y{2}/.test(format)) {
+	                        format = format.replace(/y{2}/g, this.getFullYear().toString().substr(2));
+	                    }
+	                    if (/MM/.test(format)) {
+	                        format = format.replace(/MM/g, (this.getMonth() + 1).toString().padStart(2, '0'));
+	                    }
+	                    if (/dd/.test(format)) {
+	                        format = format.replace(/dd/g, this.getDate().toString().padStart(2, '0'));
+	                    }
+	                    if (/HH/.test(format)) {
+	                        format = format.replace(/dd/g, this.getHours().toString().padStart(2, '0'));
+	                    }
+	                    if (/hh/.test(format)) {
+	                        var hour = this.getHours();
+	                        format = format.replace(/dd/g, (hour < 12 ? hour : hour - 12).toString().padStart(2, '0'));
+	                    }
+	                    if (/mm/.test(format)) {
+	                        format = format.replace(/mm/g, this.getMinutes().toString().padStart(2, '0'));
+	                    }
+	                    if (/ss/.test(format)) {
+	                        format = format.replace(/ss/g, this.getSeconds().toString().padStart(2, '0'));
+	                    }
+
+	                    return format;
+	                },
+	                writable: false,
+	                enumerable: false
+	            }
+	        });
+	    } (Date));
+	};
+
+/***/ }
+/******/ ]);
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-'use strict';
-
 (function (angular) {
     var ngApp = angular.module('ngGdeic', ['ngAnimate', 'ui.bootstrap', 'angular-linq']);
+
+    require('./src/prototype')(angular);
+
 } (angular));
